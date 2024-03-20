65aa9a543fcf5be6ad2f869a5ea5425d75d73898
Billy Ma <MaaaBilly@gmail.com>
2024-03-19 19:38:34 -0700

    menu routing

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/menu" element={<Menu />} />
+        <Route path="/#menu" element={<Menu />} />
       </Routes>
     </Router>
   </React.StrictMode>
