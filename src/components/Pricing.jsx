62b18c3e681ece2e742181fdd6a5286b4c18a2db
Billy Ma <MaaaBilly@gmail.com>
2024-04-03 16:15:54 -0700

    contact button in pricings sections

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,6 +5,37 @@ import { barista, delivery, stand5 } from '../assets';
 import { LuCheckCircle2 } from 'react-icons/lu';
 
 const Pricing = () => {
+  const encodedEmail = String.fromCharCode(
+    116,
+    97,
+    114,
+    111,
+    115,
+    98,
+    111,
+    98,
+    97,
+    98,
+    97,
+    114,
+    64,
+    103,
+    109,
+    97,
+    105,
+    108,
+    46,
+    99,
+    111,
+    109
+  );
+
+  const baristaMailto =
+    '?subject=Premium%20On%20Site%20Barista%20Inquiry&body=Hello%20Tea%20Lovers!%0A%0APlease%20share%20with%20us%20some%20details%20regarding%20the%20event%20you%20are%20planning!%0A%0ALocation%3A%20%20%0ADate%3A%20%20%0ATime%3A%20%20%0AHours%20of%20Service%3A%20%20%0AType%20of%20Event%3A%20%20%0AEstimated%20Guests%20Amount%3A%20%20%0AAdditional%20Details%3A%0A%20%20%20%20%0AThank%20you%20so%20much%20for%20reaching%20out%20to%20Tarosbobabar!%20%0A%0AWe%20will%20get%20back%20to%20you%20as%20soon%20as%20possible!';
+
+  const deliveryMailto =
+    '?subject=Delivery%20Drop%20Off%20Inquiry&body=Hello%20Tea%20Lovers!%0A%0APlease%20share%20with%20us%20some%20details%20regarding%20the%20event%20you%20are%20planning!%0A%0ALocation%3A%20%20%0ADate%3A%20%20%0ATime%20of%20Day%3A%20%20%0ANumber%20of%20Drinks%3A%20%20%0AAdditional%20Details%3A%0A%0AThank%20you%20so%20much%20for%20reaching%20out%20to%20Tarosbobabar!%20%0A%0AWe%20will%20get%20back%20to%20you%20as%20soon%20as%20possible!';
+
   return (
     <div
       className="relative mx-auto w-full pt-[4rem] pb-[6rem] md:pb-[7rem] lg:pb-[9rem] px-4 bg-pricing lg:bg-cover bg-contain bg-bottom bg-no-repeat bottom-[-10px]"
@@ -31,7 +62,7 @@ const Pricing = () => {
                   />
                 </div>
 
-                <div className="flex justify-start flex-col flex-1 px-10 pl-2 md:pl-10">
+                <div className="flex justify-start flex-col gap-2 flex-1 px-10 pl-2 md:pl-10">
                   <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
                     Premium On Site Barista
                   </h4>
@@ -52,6 +83,13 @@ const Pricing = () => {
                     guests will enjoy endless possibilities for tea flavor and
                     topping combinations!
                   </p>
+                  <Button
+                    style="secondary"
+                    href={`mailto:${encodedEmail}${baristaMailto}`}
+                    className="md:w-[180px] w-[150px] mx-auto mt-5"
+                  >
+                    Contact Us
+                  </Button>
                 </div>
               </div>
             </div>
@@ -66,7 +104,7 @@ const Pricing = () => {
                     className="rounded-3xl"
                   />
                 </div>
-                <div className="flex justify-start flex-col flex-1 w-full pl-2 md:pl-10">
+                <div className="flex justify-start flex-col gap-2  flex-1 w-full pl-2 md:pl-10">
                   <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
                     Fully Equipped Self Serve Boba Station
                   </h4>
@@ -105,8 +143,7 @@ const Pricing = () => {
                     className="rounded-3xl aspect-square"
                   />
                 </div>
-                <div className="flex justify-start flex-col flex-1 w-full pl-2 md:pl-10">
-                  {' '}
+                <div className="flex justify-start flex-col gap-2  flex-1 w-full pl-2 md:pl-10">
                   <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
                     Pre-packaged Boba Delivery
                   </h4>
@@ -135,6 +172,13 @@ const Pricing = () => {
                       $50 Delivery Fee Otherwise
                     </li>
                   </ul>
+                  <Button
+                    style="secondary"
+                    href={`mailto:${encodedEmail}${deliveryMailto}`}
+                    className="md:w-[180px] w-[150px] mx-auto mt-5"
+                  >
+                    Contact Us
+                  </Button>
                 </div>
               </div>
             </div>
