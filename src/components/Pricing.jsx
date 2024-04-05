4cb46396c22bf8d8caa693efe86fb9c75c260f3a
Billy Ma <MaaaBilly@gmail.com>
2024-04-05 13:05:31 -0700

    new Buttons, adjust Buttons placement and size for media queries

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -53,40 +53,50 @@ const Pricing = () => {
         <Tabs>
           <Tab label="On Site Barista">
             <div className="py-4">
-              <div className="flex flex-col md:flex-row items-center mb-8 p-8 overflow-hidden neumorphism">
-                <div className="flex-1 mb-4 md:mb-0 justify-center flex">
-                  <img
-                    src={barista}
-                    alt="boba milk tea catering barista"
-                    className="rounded-3xl"
-                  />
-                </div>
-
-                <div className="flex justify-start flex-col gap-2 flex-1 px-10 pl-2 md:pl-10">
-                  <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
-                    Premium On Site Barista
-                  </h4>
-                  <p className="text-2xl mt-2 ">Starting at</p>
-                  <div className="flex justify-start text-5xl md:text-6xl mb-4">
-                    <span className="mt-2 text-2xl md:text-3xl">$</span>300{' '}
-                    <span className="ml-2 self-end text-2xl md:text-3xl">
-                      /hour
-                    </span>
+              <div className="mb-8 p-8 overflow-hidden neumorphism">
+                <div className="flex flex-col md:flex-row items-center ">
+                  <div className="flex-1 mb-4 md:mb-0 justify-center flex">
+                    <img
+                      src={barista}
+                      alt="boba milk tea catering barista"
+                      className="rounded-3xl"
+                    />
                   </div>
-                  <ul className="flex flex-col gap-3 text-[1.2rem] mb-4">
-                    <li className="flex items-center gap-5">
-                      <LuCheckCircle2 size={25} /> Minimum 4 Hours Service
-                    </li>
-                  </ul>
-                  <p className="text-lg md:text-xl">
-                    We will have on site baristas making drinks to order! Your
-                    guests will enjoy endless possibilities for tea flavor and
-                    topping combinations!
-                  </p>
+                  <div className="flex justify-start flex-col gap-2 flex-1 px-10 pl-2 md:pl-10">
+                    <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
+                      Premium On Site Barista
+                    </h4>
+                    <p className="text-2xl mt-2 ">Starting at</p>
+                    <div className="flex justify-start text-5xl md:text-6xl mb-4">
+                      <span className="mt-2 text-2xl md:text-3xl">$</span>300{' '}
+                      <span className="ml-2 self-end text-2xl md:text-3xl">
+                        /hour
+                      </span>
+                    </div>
+                    <ul className="flex flex-col gap-3 text-[1.2rem] mb-4">
+                      <li className="flex items-center gap-5">
+                        <LuCheckCircle2 size={25} /> Minimum 4 Hours Service
+                      </li>
+                    </ul>
+                    <p className="text-lg md:text-xl">
+                      We will have on site baristas making drinks to order! Your
+                      guests will enjoy endless possibilities for tea flavor and
+                      topping combinations!
+                    </p>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center gap-10 mt-10">
                   <Button
                     style="secondary"
+                    href="https://tarosbobabar.netlify.app/menu"
+                    className="w-[150px] md:w-[200px] md:h-[55px] lg:w-[220px]"
+                  >
+                    View Menu
+                  </Button>
+                  <Button
+                    style="primary"
                     href={`mailto:${encodedEmail}${baristaMailto}`}
-                    className="md:w-[180px] w-[150px] mx-auto mt-5"
+                    className="w-[150px] md:w-[200px] md:h-[55px] lg:w-[220px]"
                   >
                     Contact Us
                   </Button>
@@ -96,86 +106,115 @@ const Pricing = () => {
           </Tab>
           <Tab label="Self Serve Station">
             <div className="py-4">
-              <div className="flex flex-col md:flex-row items-center mb-8 p-8 overflow-hidden neumorphism">
-                <div className="flex-1 mb-4 md:mb-0">
-                  <img
-                    src={stand5}
-                    alt="boba catering private event table station set up"
-                    className="rounded-3xl"
-                  />
-                </div>
-                <div className="flex justify-start flex-col gap-2  flex-1 w-full pl-2 md:pl-10">
-                  <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
-                    Fully Equipped Self Serve Boba Station
-                  </h4>
-                  <p className="text-2xl mt-2 ">Starting at</p>
-                  <div className="flex justify-start text-5xl md:text-6xl mb-4">
-                    <span className="mt-2 text-2xl md:text-3xl">$</span>400
+              <div className="mb-8 p-8 overflow-hidden neumorphism">
+                <div className="flex flex-col md:flex-row items-center ">
+                  <div className="flex-1 mb-4 md:mb-0">
+                    <img
+                      src={stand5}
+                      alt="boba catering private event table station set up"
+                      className="rounded-3xl"
+                    />
                   </div>
-                  <ul className="flex flex-col gap-3 text-lg mb-4">
-                    <li className="flex items-center gap-5">
-                      <LuCheckCircle2 size={25} /> Choice of Multiple Flavors
-                    </li>
-                    <li className="flex items-center gap-5">
-                      <LuCheckCircle2 size={25} />
-                      Aesthetic Decorations
-                    </li>
-                    <li className="flex items-center gap-5">
-                      <LuCheckCircle2 size={25} />
-                      Set Up and Clean Up
-                    </li>
-                  </ul>
-                  <p className="text-lg md:text-xl">
-                    Your guests will have the freedom to mix and match different
-                    teas and toppings to their liking!
-                  </p>
+                  <div className="flex justify-start flex-col gap-2  flex-1 w-full pl-2 md:pl-10">
+                    <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
+                      Fully Equipped Self Serve Boba Station
+                    </h4>
+                    <p className="text-2xl mt-2 ">Starting at</p>
+                    <div className="flex justify-start text-5xl md:text-6xl mb-4">
+                      <span className="mt-2 text-2xl md:text-3xl">$</span>400
+                    </div>
+                    <ul className="flex flex-col gap-3 text-lg mb-4">
+                      <li className="flex items-center gap-5">
+                        <LuCheckCircle2 size={25} /> Choice of Multiple Flavors
+                      </li>
+                      <li className="flex items-center gap-5">
+                        <LuCheckCircle2 size={25} />
+                        Aesthetic Decorations
+                      </li>
+                      <li className="flex items-center gap-5">
+                        <LuCheckCircle2 size={25} />
+                        Set Up and Clean Up
+                      </li>
+                    </ul>
+                    <p className="text-lg md:text-xl">
+                      Your guests will have the freedom to mix and match
+                      different teas and toppings to their liking!
+                    </p>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center gap-10 mt-10">
+                  <Button
+                    style="secondary"
+                    href="https://tarosbobabar.netlify.app/menu"
+                    className="w-[150px] md:w-[200px] md:h-[55px] lg:w-[220px]"
+                  >
+                    View Menu
+                  </Button>
+                  <Button
+                    style="primary"
+                    href="https://tarosbobabar.paperform.co"
+                    className="w-[150px] md:w-[200px] md:h-[55px] lg:w-[220px]"
+                  >
+                    Book Now
+                  </Button>
                 </div>
               </div>
             </div>
           </Tab>
           <Tab label="Delivery Drop Off">
             <div className="py-4">
-              <div className="flex flex-col md:flex-row items-center mb-8 p-8 overflow-hidden neumorphism">
-                <div className="flex-1 mb-4 md:mb-0">
-                  <img
-                    src={delivery}
-                    alt="boba catering delivery driver"
-                    className="rounded-3xl aspect-square"
-                  />
-                </div>
-                <div className="flex justify-start flex-col gap-2  flex-1 w-full pl-2 md:pl-10">
-                  <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
-                    Pre-packaged Boba Delivery
-                  </h4>
-                  <p className="text-2xl mt-2 ">Starting at</p>
-                  <div className="flex justify-start text-5xl md:text-6xl mb-4">
-                    <span className="mt-2 text-2xl md:text-3xl">$</span>6{' '}
-                    <span className="ml-2 self-end text-2xl md:text-3xl">
-                      /drink
-                    </span>
+              <div className="mb-8 p-8 overflow-hidden neumorphism">
+                <div className="flex flex-col md:flex-row items-center ">
+                  <div className="flex-1 mb-4 md:mb-0">
+                    <img
+                      src={delivery}
+                      alt="boba catering delivery driver"
+                      className="rounded-3xl aspect-square"
+                    />
                   </div>
-                  <ul className="flex flex-col gap-3 text-lg mb-4">
-                    <li className="flex items-center gap-5">
-                      <LuCheckCircle2 size={25} /> Additional 50&cent; for
-                      Toppings
-                    </li>
-                    <li className="flex items-center gap-5">
-                      <LuCheckCircle2 size={25} />
-                      30 Drinks Order Minimum
-                    </li>
-                    <li className="flex items-center gap-5">
-                      <LuCheckCircle2 size={25} />
-                      Free Delivery for 50+ Drinks
-                    </li>
-                    <li className="flex items-center gap-5">
-                      <LuCheckCircle2 size={25} />
-                      $50 Delivery Fee Otherwise
-                    </li>
-                  </ul>
+                  <div className="flex justify-start flex-col gap-2  flex-1 w-full pl-2 md:pl-10">
+                    <h4 className="hidden lg:block text-2xl md:text-3xl font-medium mb-2">
+                      Pre-packaged Boba Delivery
+                    </h4>
+                    <p className="text-2xl mt-2 ">Starting at</p>
+                    <div className="flex justify-start text-5xl md:text-6xl mb-4">
+                      <span className="mt-2 text-2xl md:text-3xl">$</span>6{' '}
+                      <span className="ml-2 self-end text-2xl md:text-3xl">
+                        /drink
+                      </span>
+                    </div>
+                    <ul className="flex flex-col gap-3 text-lg mb-4">
+                      <li className="flex items-center gap-5">
+                        <LuCheckCircle2 size={25} /> Additional 50&cent; for
+                        Toppings
+                      </li>
+                      <li className="flex items-center gap-5">
+                        <LuCheckCircle2 size={25} />
+                        30 Drinks Order Minimum
+                      </li>
+                      <li className="flex items-center gap-5">
+                        <LuCheckCircle2 size={25} />
+                        Free Delivery for 50+ Drinks
+                      </li>
+                      <li className="flex items-center gap-5">
+                        <LuCheckCircle2 size={25} />
+                        $50 Delivery Fee Otherwise
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center gap-10 mt-10">
                   <Button
                     style="secondary"
+                    href="https://tarosbobabar.netlify.app/menu"
+                    className="w-[150px] md:w-[200px] md:h-[55px] lg:w-[220px]"
+                  >
+                    View Menu
+                  </Button>
+                  <Button
+                    style="primary"
                     href={`mailto:${encodedEmail}${deliveryMailto}`}
-                    className="md:w-[180px] w-[150px] mx-auto mt-5"
+                    className="w-[150px] md:w-[200px] md:h-[55px] lg:w-[220px]"
                   >
                     Contact Us
                   </Button>
@@ -185,20 +224,20 @@ const Pricing = () => {
           </Tab>
         </Tabs>
         <div className="w-full flex justify-center gap-10">
-          <Button
+          {/* <Button
             style="secondary"
             href="https://tarosbobabar.netlify.app/menu"
             className="md:w-[180px] w-[150px]"
           >
             View Menu
-          </Button>
-          <Button
+          </Button> */}
+          {/* <Button
             style="primary"
             href="https://tarosbobabar.paperform.co"
             className="md:w-[180px] w-[150px]"
           >
             Book Now
-          </Button>
+          </Button> */}
         </div>
       </div>
     </div>
